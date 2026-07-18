6173306f6c216c95b49aa1ddec8f80d7fcaf8b8e
zmeyoy-glitch <zmeyoy@gmail.com>
2026-07-19 00:04:07 +0300

    feat: добавить корневой build.gradle.kts для проекта

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,23 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.android")
+    
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
